7072e402d2c71ef1a192e0e1e937e3424ca40f87
JoaoLCordeiro <joaolucascordeiro@gmail.com>
2019-10-16 00:25:46 -0300

    upando só o .c praticamente terminado para poder terminar de escrever o .txt

--- HilbertCurves.c
+++ HilbertCurves.c
@@ -131,10 +131,171 @@ void SubstituiExtremos(char *vetor, int i,int j1)
 	vetor[n-1]='0';
 }
 
+void VAtoDRUL (char *vetor,char **matriz,int n,int j)
+{
+	if ((j % 2) == 1)						/*if que inicializa a matriz em razão da ordem da matriz requerida*/
+	{
+		matriz[n-1][0]='U';
+	}
+	else
+	{
+		matriz[n-1][0]='R';
+	}
+	int l=n-1;
+	int c=0;
+	int i;
+	for ( i=1 ; i<(n*n) ; i++ )
+	{
+		if (matriz[l][c]=='U')					/*os ifs grandes testam qual o elemento do local atual da matriz*/
+		{
+			l=l-1;						/*dentro de cada if grande, ele desloca o local atual*/
+			if (vetor[i]=='0')				/*os ifs menores testam o elemento no vetor angular e dão o elemento atual da matriz DRUL*/
+			{
+				matriz[l][c]='U';
+			}
+			else
+			{
+			if (vetor[i]=='D')
+			{
+				matriz[l][c]='R';
+			}
+			else /* if (vetor[i]=='E') */
+			{
+				matriz[l][c]='L';
+			}
+			}				
+		}
+		else
+		{
+		if (matriz[l][c]=='D')
+		{
+			l=l+1;
+			if (vetor[i]=='0')
+                        {
+				matriz[l][c]='D';
+                        }
+                        else
+                        {
+                        if (vetor[i]=='D')
+                        {
+				matriz[l][c]='L';
+                        }
+                        else /* if (vetor[i]=='E') */
+                        {
+				matriz[l][c]='R';
+			}
+			}
+		}
+		else				
+		{
+		if (matriz[l][c]=='R')
+		{
+			c=c+1;
+			if (vetor[i]=='0')
+                        {
+				matriz[l][c]='R';
+                        }
+                        else
+                        {
+                        if (vetor[i]=='D')
+                        {
+				matriz[l][c]='D';
+                        }
+                        else /* if (vetor[i]=='E') */
+                        {
+				matriz[l][c]='U';
+                        }
+                        }				
+		}
+		else	/*if (matriz[l][c]=='L')*/
+		{
+			c=c-1;
+			if (vetor[i]=='0')
+                        {
+				matriz[l][c]='L';
+                        }
+                        else
+                        {
+                        if (vetor[i]=='D')
+                        {
+				matriz[l][c]='U';
+                        }
+                        else /* if (vetor[i]=='E') */
+                        {
+				matriz[l][c]='D';
+                        }
+                        }				
+		}
+		}
+		}
+	}
+}
+
+void ImprimirMatrizINT (int **matriz,int n)
+{
+	int i;
+	int j;
+	for ( i=0 ; i<n ; i++)
+	{
+		for ( j=0 ; j<n ; j++ )
+		{
+			printf("%d\t",matriz[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+void ImprimirMatrizCHAR (char **matriz,int n)		
+{
+	int i;
+	int j;
+	for ( i=0 ; i<n ; i++)
+	{
+		for ( j=0 ; j<n ; j++ )
+		{
+			printf("%c ",matriz[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+void DRULtoINT (char **DRUL,int **INT,int n)
+{
+	int l=n-1;
+	int c=0;
+	int i;
+	for ( i=0 ; i<n*n ; i++ )				/*passa por todos os elementos da matriz*/
+	{
+		INT[l][c]=i;					/*poe o inteiro i no local atual*/
+		if (DRUL[l][c]=='U')				/*esses ifs e elses deslocam o local atual da matriz*/
+		{
+			l=l-1;
+		}
+		else
+		{
+		if (DRUL[l][c]=='D')
+		{
+			l=l+1;
+		}
+		else
+		{
+		if (DRUL[l][c]=='R')
+		{
+			c=c+1;
+		}
+		else	/* if (DRUL[l][c]=='L') */
+		{
+			c=c-1;
+		}
+		}
+		}
+	}
+}
+
 int main ()
 {
 	int n;
-	scanf("%d",&n);
+	scanf("%d",&n);					/*lê a ordem da matriz que precisa ser gerada*/
 	float i=n/2;
 	int j=1;
 	while (i>1)					/*aqui a var i serve para testar se n é potência de 2 e j descobre o numero do exponte desa potência*/
@@ -155,7 +316,6 @@ int main ()
 		InicializaVA(va,j);				/*cria o vetor da 2x2 ou seu inverso*/
 		i=4;
 		int e=1;					/*uma espécie de boolean disfarçado*/
-		int l;
 		while (i<n2)
 		{
 			if (e==1)
@@ -171,8 +331,26 @@ int main ()
 			}
 			i=i*2;
 		}
-		ImprimeVetor(va,i);
+		/*ImprimeVetor(va,i);*/					/*tire o comentário para imprimir o vetor angular*/
 		printf("\n");
+		char **matrizDRUL = malloc (n*sizeof(char*));		/*aloca o vetor de ponteiros da DRUL*/
+		int i2;
+                for ( i2=0 ; i2<n ; i2++ )				/*aloca a matriz DRUL*/
+                {
+                	matrizDRUL[i2] = malloc (n*sizeof(char));
+		}
+		VAtoDRUL(va,matrizDRUL,n,j);				/*gera a matriz DRUL*/
+		/*ImprimirMatrizCHAR(matrizDRUL,n);*/			/*tire o comentário para imprimir a matriz DRUL */
+		free(va);						/*libera o espaço usado pelo vetor angular*/
+		int **matrizINT = malloc (n*sizeof(int*));		/*aloca o vetor de ponteiros da INT*/
+		for (i2=0 ; i2<n ; i2++ )				/*aloca a matriz INT*/
+		{
+			matrizINT[i2] = malloc (n*sizeof(int));
+		}
+		DRULtoINT(matrizDRUL,matrizINT,n);			/*passa a matriz do padrão DRUL pro INT*/
+		ImprimirMatrizINT(matrizINT,n);				/*imprime a matriz INT*/
+		free(matrizDRUL);					/*libera o espaço usado pela matriz DRUL*/
+		free(matrizINT);					/*libera o espaço usado pela matriz INT*/
 	}
 	return 0;
 }
